3b4d2619a94cb164fc79e0ded44be8a64a7d6a93
Himani Yadav <himaniyadav@google.com>
2020-06-10 14:16:40 +0000

    Make requested changes.

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -17,20 +17,19 @@
  */
 window.addEventListener('load', createMap);
 
-
 const icons = {
   image: {
-  name: 'Image Locations',
-  icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red.png'
+    name: 'Image Locations',
+    icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red.png'
   },
   place: {
-  name: 'Places To Go',
-  icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_green.png'
+    name: 'Places To Go',
+    icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_green.png'
   },
   user: {
-  name: 'User Submitted',
-  icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_purple.png'
-  }
+    name: 'User Submitted',
+    icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_purple.png'
+  },
 };
 
 /** 
@@ -41,80 +40,143 @@ function createMap() {
       document.getElementById('map'),
       {center: {lat: 36.9, lng: -76.66}, zoom: 5.2}); 
 
-  // add hardcoded markers to visualize where each gallery image was taken
+  // Add hardcoded markers to visualize where each gallery image was taken.
   addImageLandmark(
-      map, 33.9467697, -83.3755391, 'UGA Science Library',
-      'The UGA Science Library, affectionately known as the Sci Li.')
+      map, 
+      33.9467697, -83.3755391, 
+      'UGA Science Library',
+      'The UGA Science Library, affectionately known as the Sci Li.'
+  );
   addImageLandmark(
-      map, 33.9464951, -83.3772612, 'Myers Quad',
-      'Myers Quad aka the best place on campus!')
+      map, 
+      33.9464951, -83.3772612, 
+      'Myers Quad',
+      'Myers Quad aka the best place on campus!'
+  );
   addImageLandmark(
-      map, 34.647210, -83.719380, 'Yonah Mountain',
-      'Yonah Mountain in Georgia.');
+      map, 
+      34.647210, -83.719380, 
+      'Yonah Mountain',
+      'Yonah Mountain in Georgia.'
+  );
   addImageLandmark(
-      map, 31.9857085, -81.0306078, 'UGA Marine Center',
-      'UGA Marine Center on Skidaway Island in Savannah, Georgia.');
+      map, 
+      31.9857085, -81.0306078, 
+      'UGA Marine Center',
+      'UGA Marine Center on Skidaway Island in Savannah, Georgia.'
+  );
   addImageLandmark(
-      map, 40.713050, -74.007230, 'The Met',
-      'The Metropolitan Museum of Art in NYC.');
+      map, 
+      40.713050, -74.007230, 
+      'The Met',
+      'The Metropolitan Museum of Art in NYC.'
+  );
   addImageLandmark(
-      map, 51.5033273, -0.1217317, 'London Eye',
-      'The London Eye by the Thames.');
+      map, 
+      51.5033273, -0.1217317, 
+      'London Eye',
+      'The London Eye by the Thames.'
+  );
   addImageLandmark(
-      map, 51.514510, -0.098420, 'St. Pauls Cathedral',
-      'St Paul\'s Cathedral in London.');
+      map, 
+      51.514510, -0.098420, 
+      'St. Pauls Cathedral',
+      'St Paul\'s Cathedral in London.'
+  );
   addImageLandmark(
-      map, 50.6661297, -4.7590606, 'Tintagel, Cornwall',
-      'The coast at Tintagel, Cornwall');
+      map, 
+      50.6661297, -4.7590606, 
+      'Tintagel, Cornwall',
+      'The coast at Tintagel, Cornwall'
+  );
   addImageLandmark(
-      map, 51.752776, -1.253692, 'Oxford, England',
+      map, 
+      51.752776, 1.253692, 
+      'Oxford, England',
       'The view of Oxford from atop the University Church ' + 
-      'of St Mary the Virgin.');
+      'of St Mary the Virgin.'
+  );
   addImageLandmark(
-      map, 51.1462534, -2.7157123, 'King Arthur\s Tomb',
-      'Glastonbury Abbey by King Arthur\'s Tomb.');
+      map, 
+      51.1462534, -2.7157123, 
+      'King Arthur\s Tomb',
+      'Glastonbury Abbey by King Arthur\'s Tomb.'
+  );
   addImageLandmark(
-      map, 48.8594193, 2.3004549, 'Eiffel Tower',
-      'A side street near the Eiffel Tower.');
+      map, 
+      48.8594193, 2.3004549, 
+      'Eiffel Tower',
+      'A side street near the Eiffel Tower.'
+  );
   addImageLandmark(
-      map, 48.8552017, 2.3150708, 'Rodin Museum',
-      'The Thinker Statue at the Rodin Museum in Paris.');
+      map, 
+      48.8552017, 2.3150708, 
+      'Rodin Museum',
+      'The Thinker Statue at the Rodin Museum in Paris.'
+  );
   addImageLandmark(
       map, 48.8322871, 2.3846624, 'Paris Shops',
-      'Outdoor shop in Paris.');
+      'Outdoor shop in Paris.'
+  );
   addImageLandmark(
-    map, 41.2465819,1.8223518, 'Sitges, Spain',
-    'Near our stay in Sitges, Spain.');
+      map, 
+      41.2465819,1.8223518, 
+      'Sitges, Spain',
+      'Near our stay in Sitges, Spain.'
+  );
   addImageLandmark(
-    map, 52.3668475, 4.891357, 'Floating Flower Market',
-    'The floating flower market in Amsterdam.');
+      map, 
+      52.3668475, 4.891357, 
+      'Floating Flower Market',
+      'The floating flower market in Amsterdam.'
+  );
   addImageLandmark(
-    map, 52.3773409, 4.8987192, 'Basilica in Amsterdam',
-    'By the canals with a view of the Basilica in Central Amsterdam.');
+      map, 
+      52.3773409, 4.8987192, 
+      'Basilica in Amsterdam',
+      'By the canals with a view of the Basilica in Central Amsterdam.'
+  );
 
-  // add additional hardcoded markers with places I want to visit someday
+  // Add additional hardcoded markers with places I want to visit someday.
   addPlaceMarker(
-    map, 31.6003468, -8.0824187, 'Marrakesh',
-    'Though my last spring break trip to Morocco was cancelled, ' +
-    'I hope I can go again next spring.');
+    map, 
+      31.6003468, -8.0824187, 
+      'Marrakesh',
+      'Though my last spring break trip to Morocco was cancelled, ' +
+      'I hope I can go again next spring.'
+  );
   addPlaceMarker(
-    map, -13.1632509, -72.5452093, 'Machu Picchu',
-    'Seeing Machu Picchu is on my bucket list. ' + 
-    'Visiting Peru would be so cool!');
+      map, 
+      -13.1632509, -72.5452093, 
+      'Machu Picchu',
+      'Seeing Machu Picchu is on my bucket list. ' + 
+      'Visiting Peru would be so cool!'
+  );
   addPlaceMarker(
-    map, -8.5525233, 114.8082374, 'Bali, Indonesia',
-    'I would love to visit Bali one day!');
+      map, -8.5525233, 114.8082374, 
+      'Bali, Indonesia',
+      'I would love to visit Bali one day!'
+  );
   addPlaceMarker(
-    map, 26.4474128, 80.198301, 'Kanpur, India',
-    'My extended family lives in India - I haven\'t visited in a while!');
+      map, 
+      26.4474128, 80.198301, 
+      'Kanpur, India',
+      'My extended family lives in India - I haven\'t visited in a while!'
+  );
   addPlaceMarker(
-    map, 37.4220041, -122.0862462, 'Googleplex Mountain View',
-    'I\'ve never seen Google\'s Mountain View HQ before.');
+      map, 
+      37.4220041, -122.0862462, 
+      'Googleplex Mountain View',
+      'I\'ve never seen Google\'s Mountain View HQ before.'
+  );
   addPlaceMarker(
-    map, 40.4571004, -79.9171171, 'Google Pittsburgh Office',
-    'My current team is based in Pittsburgh. I\'d love to visit Google PIT!');
+      map, 
+      40.4571004, -79.9171171, 
+      'Google Pittsburgh Office',
+      'My current team is based in Pittsburgh. I\'d love to visit Google PIT!'
+  );
 
-  // create and display legend of icons
+  // Create and display legend of icons.
   let legend = document.getElementById('legend');
   for (var key in icons) {
     let type = icons[key];
@@ -130,6 +192,7 @@ function createMap() {
 /** 
  * Adds a marker that shows an info window when clicked.
  * Represents a place pictured in the image gallery.
+ * @param {google.maps.Map} map Manages the state of the visual Google Map
  */
 function addImageLandmark(map, lat, lng, title, description) {
   const marker = new google.maps.Marker(
@@ -144,6 +207,7 @@ function addImageLandmark(map, lat, lng, title, description) {
 /** 
  * Adds a marker that shows an info window when clicked.
  * Represents a place I have not yet visited.
+ * @param {google.maps.Map} map Manages the state of the visual Google Map
  */
 function addPlaceMarker(map, lat, lng, title, description) {
   const marker = new google.maps.Marker({
