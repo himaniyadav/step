a8cb069642576bc593d751f23c964538a10289bd
Himani Yadav <himaniyadav@google.com>
2020-06-11 16:54:55 +0000

    Modify login servlet to only return json.

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -34,60 +34,30 @@ public class LoginServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    PrintWriter out = response.getWriter();
-    out.println("<h1>Login Page</h1>");
-
-    // Only logged-in users can see the form
     UserService userService = UserServiceFactory.getUserService();
+
+    String userEmail = userService.getCurrentUser().getEmail();
+    String loginUrl = userService.createLoginURL("/");
+    String logoutUrl = userService.createLogoutURL("/");
+
+    String json = "{";
     if (userService.isUserLoggedIn()) {
-      out.println("<p>Hello " + userService.getCurrentUser().getEmail() + "!</p>");
-      out.println("<p>Type a message and click submit:</p>");
-      out.println("<form method=\"POST\" action=\"/login\">");
-      out.println("<textarea name=\"text\"></textarea>");
-      out.println("<br/>");
-      out.println("<button>Submit</button>");
-      out.println("</form>");
+      json += "\"email\": ";
+      json += "\"" + userService.getCurrentUser().getEmail() + "\"";
+      json += ", ";
+      json += "\"url\": ";
+      json += "\"" + logoutUrl + "\"";
     } else {
-      String loginUrl = userService.createLoginURL("/login");
-      out.println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+      json += "\"email\": ";
+      json += "\"null\"";
+      json += ", ";
+      json += "\"url\": ";
+      json += "\"" + loginUrl + "\"";
     }
+    json += "}";
 
-    // Everybody can see the messages
-    out.println("<ul>");
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
-    PreparedQuery results = datastore.prepare(query);
-    for (Entity entity : results.asIterable()) {
-      String text = (String) entity.getProperty("text");
-      String email = (String) entity.getProperty("email");
-      out.println("<li>" + email + ": " + text + "</li>");
-    }
-    out.println("</ul>");
-  }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-    UserService userService = UserServiceFactory.getUserService();
-
-    // Only logged-in users can post messages
-    if (!userService.isUserLoggedIn()) {
-      response.sendRedirect("/login");
-      return;
-    }
-
-    String text = request.getParameter("text");
-    String email = userService.getCurrentUser().getEmail();
-
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Entity messageEntity = new Entity("Message");
-    messageEntity.setProperty("text", text);
-    messageEntity.setProperty("email", email);
-    messageEntity.setProperty("timestamp", System.currentTimeMillis());
-    datastore.put(messageEntity);
-
-    // Redirect to /login. The request will be routed to the doGet() function above.
-    response.sendRedirect("/login");
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
