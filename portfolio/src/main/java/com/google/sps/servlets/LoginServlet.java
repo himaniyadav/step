c14dbbe15588153ed911f011836b3eb75f1ce253
Himani Yadav <himaniyadav@google.com>
2020-06-11 16:56:43 +0000

    Remove unnecessary imports.

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -14,12 +14,6 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
